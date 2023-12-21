2ad75e5f4d7f97ec7642626d3dbdae2e784a777d
Ken Takahashi <takaken@redimpulz.com>
2023-12-21 15:34:22 +0900

    テストのディレクトリを修正

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,5 @@ import { test } from "vitest";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: { dir: "__test__/unit" },
+  test: { dir: "__tests__/unit" },
 });
